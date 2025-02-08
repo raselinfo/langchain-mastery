3802a09ecbd092d9fb3bcde599e2d2205aec18cf
Rasel Hossain <141147182+rasel-stacklearner@users.noreply.github.com>
2025-02-08 13:10:42 +0600

    add langchain eco-system code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-import {} from "@langchain/core/prompts";
+import {
+  ChatMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+  PromptTemplate,
+} from "@langchain/core/prompts";
+
+import { createRetrievalChain } from "langchain/chains/retrieval";
+
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
+import { ChatOpenAI } from "@langchain/openai";
+
+import { ChatAnthropic } from "@langchain/anthropic";
